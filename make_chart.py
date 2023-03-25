6e3de166794875a3b2212ec8b16ac5ec078071c3
jgumbley <jgumbley@thoughtworks.com>
2023-03-25 11:30:54 +0000

    tweaks to formating

--- make_chart.py
+++ make_chart.py
@@ -13,10 +13,12 @@ countries = ['Germany', 'Finland', 'United Kingdom']
 weekly_cases_file = 'weekly_cases_per_million.csv'
 weekly_deaths_file = 'weekly_deaths_per_million.csv'
 excess_mortality_file = 'excess_mortality.csv'
+icu_occupancy_file = 'covid-hospitalizations.csv'
 
 weekly_cases_data = pd.read_csv(weekly_cases_file)
 weekly_deaths_data = pd.read_csv(weekly_deaths_file)
 excess_mortality_data = pd.read_csv(excess_mortality_file)
+icu_occupancy_data = pd.read_csv(icu_occupancy_file)
 
 
 """Take the timestamp of data, and convert it into a pandas timestamp """
@@ -26,11 +28,12 @@ excess_mortality_data = pd.read_csv(excess_mortality_file)
 weekly_cases_data[date_field_name] = pd.to_datetime(weekly_cases_data[date_field_name], format=date_format)
 weekly_deaths_data[date_field_name] = pd.to_datetime(weekly_deaths_data[date_field_name], format=date_format)
 excess_mortality_data[date_field_name] = pd.to_datetime(excess_mortality_data[date_field_name], format=date_format)
+icu_occupancy_data[date_field_name] = pd.to_datetime(icu_occupancy_data[date_field_name], format=date_format)
 
 weekly_cases_data.set_index(date_field_name, inplace=True)
 weekly_deaths_data.set_index(date_field_name, inplace=True)
 excess_mortality_data.set_index(date_field_name, inplace=True)
-
+icu_occupancy_data.set_index(date_field_name, inplace=True)
 
 """ Take a look at excess deaths data """
 
@@ -39,13 +42,20 @@ special_list=['Germany', 'Finland', 'United Kingdom']
 excess_value="cum_excess_per_million_proj_all_ages"
 selected_countries_data = excess_mortality_data[excess_mortality_data['location'].isin(special_list)].pivot_table(values=excess_value, index='date', columns='location')
 
-print(selected_countries_data)
+""" Take a look at ICU data"""
+
+# Filter the data for the required countries and the indicator "Daily ICU occupancy per million"
+selected_icu_occupancy_data = icu_occupancy_data[
+    (icu_occupancy_data['entity'].isin(countries)) & 
+    (icu_occupancy_data['indicator'] == 'Daily ICU occupancy per million')
+].pivot_table(values='value', index='date', columns='entity')
+
 
 """ Merge deaths and cases weekly data"""
 
 merged_data = weekly_cases_data.merge(weekly_deaths_data, left_index=True, right_index=True, suffixes=('_cases', '_deaths'))
 
-print(merged_data)
+
 
 """Select data just for our country"""
 """Create a plotly plot (or graph configured how we want it)"""
@@ -57,9 +67,9 @@ colors = ['red', 'green', 'blue']
 
 
 
-fig = make_subplots(rows=3, cols=1,
-                    specs=[[{"secondary_y": False}], [{"secondary_y": False}], [{"secondary_y": False}]],
-                    subplot_titles=("Weekly Cases per Million", "Weekly Deaths per Million", "Excess Mortality per million"))
+fig = make_subplots(rows=4, cols=1,
+                    specs=[[{"secondary_y": False}], [{"secondary_y": False}], [{"secondary_y": False}], [{"secondary_y": False}]],
+                    subplot_titles=("Weekly Cases per Million", "Weekly Deaths per Million", "Excess Mortality per Million", "Daily ICU Occupancy per Million"))
 
 
 # Add traces for weekly cases per million
@@ -84,8 +94,34 @@ for i, country in enumerate(countries):
         row=3, col=1
     )
 
+# Step 4: Plot the ICU occupancy data as a time series
+for i, country in enumerate(countries):
+    fig.add_trace(
+        go.Scatter(x=selected_icu_occupancy_data.index, y=selected_icu_occupancy_data[country], showlegend=False, line=dict(color=colors[i])),
+        row=4, col=1
+    )
+
+
 # Update layout and subplot titles
-fig.update_layout(title='Time Series Comparison', showlegend=True, height=800)
+fig.update_layout(
+    title=dict(
+        text='Comparing health consequences of COVID-19 between Germany, UK and Finland',
+        font=dict(
+            size=24,
+            family='Georgia',
+            color='darkblue'
+        )
+    ),
+    showlegend=True,
+    height=1000,
+    legend=dict(
+        orientation='h',
+        yanchor='bottom',
+        y=-0.2,
+        xanchor='center',
+        x=0.5
+    )
+)
 
 #fig.update_yaxes(type='log', row=3, col=1)
 fig.update_xaxes(range=['2020-01-01', '2023-01-01'])
